fe10a4289099aea93f135be28aff4872fedbbc8a
CheesyFrappe <emirbalci360@gmail.com>
2023-12-30 12:04:03 +0300

    Add 'math' directory and #9

---
+++ python3/math/palindromeNumber.py
@@ -0,0 +1,21 @@
+"""
+    9 - Palindrome Number - Easy
+    https://leetcode.com/problems/palindrome-number
+    Topics: Math
+
+    Runtime complexity: O(n)
+    Spacetime complexity: O(1)
+"""
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x > 0 and x % 10 == 0):
+            return False
+        
+        res = 0
+
+        while x > res:
+            res = res * 10 + x % 10
+            x //= 10
+        
+        return x == res or x == res // 10
